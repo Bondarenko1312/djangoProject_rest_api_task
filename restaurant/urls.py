5cc2a6cbcae0129f473a0575372ae734b3dfd23a
Bondarenko1312 <bondarenkoroman00@gmail.com>
2022-09-17 04:29:17 +0300

    commit

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,25 +1,12 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
 
-from user.urls import router as user_routers
-from user.views import logout
-from employee.urls import router as employee_routers
-from restaurant.urls import router as restaurant_routers
+from .views import *
 
 router = DefaultRouter()
-router.registry.extend(user_routers.registry)
-router.registry.extend(employee_routers.registry)
-router.registry.extend(restaurant_routers.registry)
+router.register('restaurant', RestaurantViewSet, basename='restaurant')
+router.register('restaurant-profile', RestaurantProfileViewSet, basename='restaurant-profile')
+router.register('restaurant-menu', MenuViewSet, basename='menu')
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  url(r'^api/v1/', include(router.urls)),
-                  url(r'^api/v1/logout$', logout, name="logout"),
-                  path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
